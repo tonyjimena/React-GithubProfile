4917918daa6ad5d2cc9985c06ee7f1f2fea94bfe
tonyjimena <jimenaalvarez.tony@gmail.com>
2022-03-27 17:37:48 +0200

    3 files modified

--- components/OverViewNav.js
+++ components/OverViewNav.js
@@ -1,22 +1,37 @@
 import React from 'react';
 
-import { Link, Route } from 'wouter';
+import { Link, Route, useRoute } from 'wouter';
+import { Transition } from 'react-transition-group';
 
 import UserGitRepos from './UserGitRepos';
 import UserOverview from './UserOverview';
 
 export default function OverViewNav() {
+  const [match, params] = useRoute('/repositories');
+
   return (
-    <div>
-      <Link href="/repositories">
-        <a className="link">Repositories</a>
-      </Link>
-      <Link href="/">
-        <a className="link">Overview</a>
-      </Link>
+    <>
+      <div className="overview-navigation">
+        <Link href="/">
+          <a className="link current">Overview</a>
+        </Link>
+
+        <Link href="/repositories">
+          <a className="link">Repositories</a>
+        </Link>
+      </div>
 
       <Route path="/" component={UserOverview} />
-      <Route path="/repositories" component={UserGitRepos} />
-    </div>
+
+      <Transition
+        transitionName="example"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}
+      >
+        <Route path="/repositories" component={UserGitRepos} />
+      </Transition>
+    </>
   );
 }
